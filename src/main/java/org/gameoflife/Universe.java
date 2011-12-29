200a0f6c30b3f72e9fe87247a9b88be144e18d76
Adelina Caramet <adelina.caramet@gmail.com>
2011-12-29 17:04:24 +0200

    cleanup

--- src/main/java/org/gameoflife/Universe.java
+++ src/main/java/org/gameoflife/Universe.java
@@ -8,33 +8,14 @@ public class Universe {
         this.cells = cells;
     }
 
-
     public void turn() {
         Boolean[][] nextState = new Boolean[cells.length][cells[cells.length-1].length];
         for(int row = 0; row < cells.length; row++) {
             for(int col = 0; col < cells[cells.length-1].length; col++) {
-                nextState[row][col] = isAliveNextTurn(cells[row][col], getNeighbours(row, col));
+                nextState[row][col] = isAliveNextTurn(cells[row][col], getNeighboursForCellAt(row, col));
             }
         }
         setCells(nextState);
-    }
-
-    public Boolean[][] getCells() {
-        return cells;
-    }
-
-    public void setCells(Boolean[][] cells) {
-        this.cells = cells;
-    }
-
-    public boolean getCell(int row, int column) {
-        if (row < 0 || column < 0) {
-            return false;
-        }
-        if (row >= cells.length || column >= cells[cells.length - 1].length) {
-            return false;
-        }
-        return cells[row][column];
     }
 
     public Boolean isAliveNextTurn(Boolean cell, Boolean[] neighbours) {
@@ -45,7 +26,7 @@ public class Universe {
         return (!cell && count == 3) || (cell && count == 2) || (cell && count == 3);
     }
 
-    public Boolean[] getNeighbours(int row, int column) {
+    private Boolean[] getNeighboursForCellAt(int row, int column) {
         Boolean[] neighbours = new Boolean[8];
         neighbours[0] = getCell(row - 1, column - 1);
         neighbours[1] = getCell(row - 1, column);
@@ -56,5 +37,23 @@ public class Universe {
         neighbours[6] = getCell(row + 1, column - 1);
         neighbours[7] = getCell(row, column - 1);
         return neighbours;
+    }
+
+    private boolean getCell(int row, int column) {
+        if (row < 0 || column < 0) {
+            return false;
+        }
+        if (row >= cells.length || column >= cells[cells.length - 1].length) {
+            return false;
+        }
+        return cells[row][column];
+    }
+
+    public Boolean[][] getCells() {
+        return cells;
+    }
+
+    public void setCells(Boolean[][] cells) {
+        this.cells = cells;
     }
 }
--- src/test/java/org/gameoflife/UniverseTest.java
+++ src/test/java/org/gameoflife/UniverseTest.java
@@ -24,20 +24,13 @@ public class UniverseTest {
             {false, true, false},
     };
 
-    @Test
-    public void cellsHaveInitialState() {
-        Assert.assertArrayEquals(initialState, universe.getCells());
+    @Before
+    public void init() {
+        universe = new Universe(initialState);
     }
 
     @Test
-    public void iCanAccessCell() {
-        Assert.assertFalse(universe.getCell(1, 0));
-        Assert.assertTrue(universe.getCell(1, 2));
-
-    }
-
-    @Test
-    public void aDeadCellWithExactThreeNeighboursComesAlive() {
+    public void aDeadCellWithExactlyThreeNeighboursComesAlive() {
         Boolean cell = false;
         Boolean[] neighbours = new Boolean[]{true, true, true, false, false, false, false, false};
         Boolean state = universe.isAliveNextTurn(cell, neighbours);
@@ -69,41 +62,15 @@ public class UniverseTest {
     }
 
     @Test
-    public void extractNeighboursForMiddleCell() {
-        Boolean[] neighbours = new Boolean[]{true, true, true, true, true, true, false, false};
-        Boolean[] realNeighbours = universe.getNeighbours(1, 1);
-        Assert.assertArrayEquals(neighbours, realNeighbours);
-    }
-
-    @Test
-    public void extractNeighboursForCellAtTheUpperBorder() {
-        Boolean[] neighbours = new Boolean[]{false, false, false, true, false, false, false, false};
-        Boolean[] realNeighbours = universe.getNeighbours(0, 0);
-        Assert.assertArrayEquals(neighbours, realNeighbours);
-    }
-
-    @Test
-    public void extractNeigboursForCellAtTheBottomBorder() {
-        Boolean[] neighbours = new Boolean[]{false, true, false, false, false, false, false, true};
-        Boolean[] realNeighbours = universe.getNeighbours(2, 2);
-        Assert.assertArrayEquals(neighbours, realNeighbours);
-    }
-
-    @Test
-    public void afterTheTurnCellsHaveNewState() {
+    public void afterTheTurnCellsHaveANewState() {
         universe.turn();
         Assert.assertArrayEquals(nextState, universe.getCells());
     }
 
     @Test
-    public void afterSecondTurnCellsHaveNewState() {
+    public void afterTheSecondTurnCellsHaveANewState() {
         universe.turn();
         universe.turn();
         Assert.assertArrayEquals(nextNextState, universe.getCells());
-    }
-
-    @Before
-    public void init() {
-        universe = new Universe(initialState);
     }
 }
